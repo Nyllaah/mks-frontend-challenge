f959daafae60be392b85c2f7afcf23091c1905ab
Nyllaah <nylla.g@hotmail.com>
2024-04-14 16:38:52 -0300

    fix: price decimals

--- mks-app/src/components/Product.tsx
+++ mks-app/src/components/Product.tsx
@@ -1,11 +1,13 @@
 import { IProduct } from "../Interfaces";
 
 function ProductCard(props: IProduct) {
+  const formattedPrice = Math.floor(props.price);
+
   return (
     <div key={props.id}>
       <img src={props.photo} alt="" />
       <h3>{`${props.brand} ${props.name}`}</h3>
-      <span>{props.price}</span>
+      <span>{`R$${formattedPrice}`}</span>
       <p>{props.description}</p>
       <button>Comprar</button>
     </div>
