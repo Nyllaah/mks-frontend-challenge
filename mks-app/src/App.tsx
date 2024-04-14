919ed2d5b07ede60a12915f0556148b5eb6fd1a9
Nyllaah <nylla.g@hotmail.com>
2024-04-14 13:01:42 -0300

    feat: base html

--- mks-app/src/App.tsx
+++ mks-app/src/App.tsx
@@ -2,7 +2,28 @@ function App() {
 
   return (
     <>
-      <h1>test</h1>
+      <header>
+        <h1>MSK</h1>
+        <h2>Sistemas</h2>
+        <div>
+        <button>carrinho</button>
+        </div>
+      </header>
+      <section>
+        <div>
+          <img src="" alt="" />
+          <h3>titulo</h3>
+          <span>price</span>
+          <p>descrição</p>
+          <button>Comprar</button>
+        </div>
+      </section>
+      <section>
+        <h3>carrinho</h3>
+      </section>
+      <footer>
+        <span>MKS sistemas © Todos os direitos reservados</span>
+      </footer>
     </>
   )
 }
