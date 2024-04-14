e2d992e2562345ce1343d06b3c0e5e25874178f2
Nyllaah <nylla.g@hotmail.com>
2024-04-14 18:18:33 -0300

    feat: header style

--- mks-app/src/App.tsx
+++ mks-app/src/App.tsx
@@ -11,7 +11,7 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-    font-family: Montserrat;
+    font-family: "Montserrat", sans-serif;
   }
 `;
 
--- mks-app/src/components/Header.tsx
+++ mks-app/src/components/Header.tsx
@@ -1,13 +1,58 @@
-function Header () {
+import styled from "styled-components";
+
+export default function Header() {
   return (
-    <header>
-      <h1>MSK</h1>
-      <h2>Sistemas</h2>
-      <div>
-      <button>carrinho</button>
-      </div>
-    </header>
+    <HeaderWrapper>
+      <TitleWrapper>
+        <Title>MSK</Title>
+        <Subtitle>Sistemas</Subtitle>
+      </TitleWrapper>
+      <CartBtn onClick={() => {}}>
+        <img src="../../public/cart.svg" alt=""/>
+        <span>0</span>
+      </CartBtn>
+    </HeaderWrapper>
   )
 }
 
-export default Header;
+const HeaderWrapper = styled.header`
+  height: 101px;
+  width: 100vw;
+  background-color: #0F52BA;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 5%;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: end;
+  gap: 6px;
+`;
+
+const Title = styled.h1`
+  color: white;
+  font-size: 40px;
+`;
+
+const Subtitle = styled.h2`
+  font-size: 20px;
+  font-weight: 300;
+  color: white;
+  line-height: 32px;
+`;
+
+const CartBtn = styled.button`
+  width: 90px;
+  height: 45px;
+  border-radius: 0.6rem;
+  font-size: 18px;
+  font-weight: 700;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
