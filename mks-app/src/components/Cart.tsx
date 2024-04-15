acd63de617783f7a4479db05739a65e8652e56ac
Nyllaah <nylla.g@hotmail.com>
2024-04-14 22:54:05 -0300

    feat: cart style

--- mks-app/src/components/Cart.tsx
+++ mks-app/src/components/Cart.tsx
@@ -2,13 +2,21 @@ import styled from "styled-components";
 import CartCard from "./CartCard";
 import { useSelector } from "react-redux";
 import { CartCardProps, StateType } from "../types";
+import { useDispatch } from "react-redux";
+import { showCart } from "../redux/actions";
 
 export default function Cart(props: {showCart: boolean}) {
   const { cartProducts } = useSelector((state: StateType) => state);
+  const dispatch = useDispatch();
+
+  const total = cartProducts.reduce((acc, curr) => acc + curr.price, 0);
 
   return (
     <CartWrapper style={ props.showCart ? { display: "flex" } : { display: "none" } }>
-      <h3>Carrinho de Compras</h3>
+      <TitleWrapper>
+        <h3>Carrinho de Compras</h3>
+        <CloseBtn onClick={ () => dispatch(showCart()) }>X</CloseBtn>
+      </TitleWrapper>
       {cartProducts.map(({id, name, brand, photo, price}: CartCardProps) => (
         <CartCard
           key={id}
@@ -20,16 +28,61 @@ export default function Cart(props: {showCart: boolean}) {
         />
       ))}
       <div>
-        <span>Total:</span>
-        <span>price</span>
+        <TotalWrapper>
+          <span>Total:</span>
+          <span>{total}</span>
+        </TotalWrapper>
+        <SubmitBtn>Finalizar Compra</SubmitBtn>
       </div>
-      <button>Finalizar Compra</button>
     </CartWrapper>
   )
 }
 
 const CartWrapper = styled.div`
-  min-width: 30vw;
-  height: 100vh;
+  width: 486px;
+  height: 100%;
   background-color: #0F52BA;
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 1000;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-size: 1.5rem;
+  font-weight: 700;
+  color: white;
+`;
+
+const CloseBtn = styled.button`
+  border-radius: 100%;
+  background-color: black;
+  color: white;
+  text-align: center;
+  width: 38px;
+  height: 38px;
+  border: none;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem;
+`;
+
+const TotalWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem;
+`;
+
+const SubmitBtn = styled.button`
+  border: none;
+  background-color: black;
+  color: white;
+  font-size: 28px;
+  font-weight: 700;
+  text-align: center;
+  width: 100%;
+  height: 5rem;
 `;
